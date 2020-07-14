075c504132f76cbd35bfbd7ad7a2937dcf1625e2
Anson Nickel <anson.nickel@gmail.com>
2020-07-14 17:59:21 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,15 @@
-# Write your methods here
+def loop_message_five_times(message)
+  counter = 0
+  while counter < 5 do
+    puts message
+    counter+=1
+end
+
+def loop_message_n_times
+end
+
+def output_array
+end
+
+def return_string_array
+end
